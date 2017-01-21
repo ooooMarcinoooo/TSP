d16324fa440dac8ae0c184c5744ff8a3f41b952a
ooooMarcinoooo <kucmarcin@o2.pl>
2017-01-21 10:56:36 +0100

    update
    update

---
+++ Cities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Globalization;
+
+namespace TSP
+{
+     public class Cities : List<City>
+    {
+         public void CalculateCityDistances( int numberOfCloseCities )
+         {
+         
+         }
+         public void OpenCityList(string fileName)
+         {
+             int[] zakres  = new int[100];
+             Random los = new Random();
+             int x, y, miejsce;
+
+             for (int i = 0; i < 9; i++)
+             {
+                 miejsce = los.Next(0, zakres.Length);
+                 x = zakres[miejsce];
+                 miejsce = los.Next(0, zakres.Length);
+                 y = zakres[miejsce];
+                 this.Add(new City(x, y));
+                 Console.WriteLine(x);
+                 Console.WriteLine(y); 
+             }
+         }   
+
+       //this.
+         //                   this.Add(new City(Convert.ToInt32(city["X"], CultureInfo.CurrentCulture), Convert.ToInt32(city["Y"], CultureInfo.CurrentCulture)));
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Globalization;
 
 namespace TSP
 {
@@ -15,6 +18,26 @@ namespace TSP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Cities cityList = new Cities();
+        private void DrawCityList(Cities cityList)
+        {
+            //Image cityImage = new Bitmap(tourDiagram.Width, tourDiagram.Height);
+            Graphics graphics = Graphics.FromImage(cityImage);
+
+            foreach (City city in cityList)
+            {
+                // Draw a circle for the city.
+                graphics.DrawEllipse(Pens.Black, city.Location.X - 2, city.Location.Y - 2, 5, 5);
+            }
+
+            //this.tourDiagram.Image = cityImage;
+
+            //updateCityCount();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
         }
     }
 }
